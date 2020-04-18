2d06f613f17fc071690c2a90447793c025302a91
kkk <koka.tirkia@gmail.com>
2020-04-18 23:35:13 +0400

    initial commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.notes.databinding.ActivityMainBinding;
+import com.example.notes.databinding.AddNoteBinding;
+import com.example.notes.database.model.Note;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+
+        binding.recId.setLayoutManager(new LinearLayoutManager(this));
+        final NoteAdapter noteRecAdapter = new NoteAdapter();
+        binding.recId.setAdapter(noteRecAdapter);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                noteRecAdapter.setNotes(notes);
+            }
+        });
+
+        noteRecAdapter.itemClickListener(new NoteAdapter.OnItemClickListener() {
+            @Override
+            public void onRemoveClick(Note note) {
+                noteViewModel.delete(note);
+            }
+        });
+
+        binding.addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNoteDialog();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.delete){
+            noteViewModel.deleteAllNotes();
+            return true;
+        }
+        return true;
+    }
+
+    private void addNoteDialog() {
+        final AddNoteBinding bindingMeter = AddNoteBinding.inflate(getLayoutInflater());
+        View view = bindingMeter.getRoot();
+
+        final AlertDialog addMeterDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .show();
+        bindingMeter.saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bindingMeter.title.getText().toString().isEmpty()
+                        || bindingMeter.description.getText().toString().isEmpty()
+                ) {
+
+                    Toast.makeText(v.getContext(), "Fill Fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Note note = new Note(
+                            bindingMeter.title.getText().toString(),
+                            bindingMeter.description.getText().toString());
+
+                    noteViewModel.insert(note);
+                    Toast.makeText(v.getContext(), "Saved", Toast.LENGTH_SHORT).show();
+                    addMeterDialog.dismiss();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/notes/NoteAdapter.java
@@ -0,0 +1,76 @@
+package com.example.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notes.database.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+    private OnItemClickListener listener;
+
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_row_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = notes.get(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private ImageButton removeNote;
+
+        public NoteHolder(View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            removeNote=itemView.findViewById(R.id.delete_note);
+            removeNote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null && getAdapterPosition() != RecyclerView.NO_POSITION) {
+                        listener.onRemoveClick(notes.get(getAdapterPosition()));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onRemoveClick(Note note);
+    }
+
+    public void itemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+}
